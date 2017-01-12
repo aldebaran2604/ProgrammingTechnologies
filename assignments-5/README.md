b55b9d283e473a056838226de98f14acad3ad00c
aldebaran2604 <luis-jose-26041987@hotmail.com>
2017-01-12 00:18:42 -0600

    programa jess java c respaldo

--- assignments-5/README.md
+++ assignments-5/README.md
@@ -8,6 +8,7 @@ gcc jni-test.c -L/usr/lib/jvm/java-8-oracle/jre/lib/amd64/server -I/usr/lib/jvm/
 
 gcc SampleRete.c -L/usr/lib/jvm/java-8-oracle/jre/lib/amd64/server -I/usr/lib/jvm/java-8-oracle/include/ -I/usr/lib/jvm/java-8-oracle/include/linux/ -L/usr/bin/java -ljvm -o SampleReteJVM
 
+gcc assignment2.c -L/usr/lib/jvm/java-8-oracle/jre/lib/amd64/server -I/usr/lib/jvm/java-8-oracle/include/ -I/usr/lib/jvm/java-8-oracle/include/linux/ -L/usr/bin/java -ljvm -o assignment2
 
 * export JAVA_HOME=/usr/lib/jvm/java-7-oracle/
 * export PATH="$PATH:/usr/lib/jvm/java-7-oracle/bin:/home/aldeberan/bin:/home/aldebaran/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin"
@@ -18,3 +19,8 @@ gcc Sample2.c -L/usr/lib/jvm/java-7-oracle/jre/lib/amd64/server -I/usr/lib/jvm/j
 gcc jni-test.c -L/usr/lib/jvm/java-7-oracle/jre/lib/amd64/server -I/usr/lib/jvm/java-7-oracle/include/ -I/usr/lib/jvm/java-7-oracle/include/linux/ -L/usr/bin/java -ljvm -o SampleNewJVM
 
 gcc SampleRete.c -L/usr/lib/jvm/java-7-oracle/jre/lib/amd64/server -I/usr/lib/jvm/java-7-oracle/include/ -I/usr/lib/jvm/java-7-oracle/include/linux/ -L/usr/bin/java -ljvm -o SampleReteJVM
+
+gcc assignment2.c -L/usr/lib/jvm/java-7-oracle/jre/lib/amd64/server -I/usr/lib/jvm/java-7-oracle/include/ -I/usr/lib/jvm/java-7-oracle/include/linux/ -L/usr/bin/java -ljvm -o assignment2
+
+
+jar xfv jess.jar desempaquetar jess para poder utilizar la clase Rete
---
+++ assignments-5/assignment2.c
@@ -0,0 +1,75 @@
+#include <jni.h>
+#include <string.h>
+//#include <stdio.h>
+#include <stdlib.h>
+
+jobject initContrcutor(char *nombreClase, jclass cls, JavaVM *jvm, JNIEnv *env);
+void addEval(char* evalCadena, jclass cls, jobject instanceObject, JavaVM *jvm, JNIEnv *env);
+
+int main()
+{
+	JavaVMOption options[1];
+	JNIEnv *env;
+	JavaVM *jvm;
+	JavaVMInitArgs vm_args;
+
+	options[0].optionString = "-Djava.class.path=.:jess/Rete";
+
+	memset(&vm_args, 0, sizeof(vm_args));
+	vm_args.version = JNI_VERSION_1_8;
+	vm_args.nOptions = 1;
+	vm_args.options = options;
+
+	long status = JNI_CreateJavaVM(&jvm, (void**)&env, &vm_args);
+
+	if (status == JNI_ERR) {
+		printf("Error al crear la maquina virtual, bye bye!\n");
+		return -1;
+	}
+	jobject instanceRete;
+	jclass clsRete;
+	instanceRete = initContrcutor("jess/Rete", clsRete, jvm, env);
+	jclass cls = (*env)->GetObjectClass(env, instanceRete);
+	addEval("(reset)", cls, instanceRete, jvm, env);
+	addEval("(reset)", cls, instanceRete, jvm, env);
+	addEval("(run)", cls, instanceRete, jvm, env);
+}
+
+jobject initContrcutor(char *nombreClase, jclass cls, JavaVM *jvm, JNIEnv *env){
+	cls = (*env)->FindClass(env, nombreClase);
+	if (cls==0) {
+		(*jvm)->DestroyJavaVM(jvm);
+	    printf("Error no se encontro la clase %s, bye bye!\n", nombreClase);
+		exit(0);
+	}
+	jmethodID constructor;
+	constructor = (*env)->GetMethodID(env, cls, "<init>", "()V");
+    if (constructor==0) {
+        (*jvm)->DestroyJavaVM(jvm);
+        printf("Error no se logro instanciar %s, bye bye!\n", nombreClase);
+        exit(0);
+    }
+    jobject simpleJNITestInstance = (*env)->NewObject(env, cls, constructor);
+    if (simpleJNITestInstance == 0) {
+        (*jvm)->DestroyJavaVM(jvm);
+        printf("Error no se logro instanciar %s, bye bye!\n", nombreClase);
+        exit(0);
+    }
+	return simpleJNITestInstance;
+}
+
+void addEval(char* evalCadena, jclass cls, jobject instanceObject, JavaVM *jvm, JNIEnv *env) {
+	if (cls==NULL) {
+		(*jvm)->DestroyJavaVM(jvm);
+		printf("Error cls, bye bye!\n");
+		exit(0);
+	}
+	jmethodID evalRete = (*env)->GetMethodID(env, cls, "eval", "(Ljava/lang/String;)Ljess/Value;");
+	jstring jstr1 = (*env)->NewStringUTF(env, evalCadena);
+    if (evalRete==0) {
+        (*jvm)->DestroyJavaVM(jvm);
+        printf("Error no se logro obtener el Metodo eval de Rete, bye bye!\n");
+        exit(0);
+    }
+    (*env)->CallObjectMethod(env, instanceObject , evalRete, jstr1);
+}
